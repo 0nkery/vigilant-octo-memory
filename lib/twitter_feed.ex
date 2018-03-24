fe898d4fd4d06ee071bd0cf106e195a125a3cadb
Dmitry Shlagoff <wessmoke@gmail.com>
2018-03-24 21:32:17 +0700

    Removed default code

--- lib/twitter_feed.ex
+++ lib/twitter_feed.ex
@@ -2,17 +2,4 @@ defmodule TwitterFeed do
   @moduledoc """
   Documentation for TwitterFeed.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TwitterFeed.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/twitter_feed_test.exs
+++ test/twitter_feed_test.exs
@@ -1,8 +1,4 @@
 defmodule TwitterFeedTest do
   use ExUnit.Case
-  doctest TwitterFeed
 
-  test "greets the world" do
-    assert TwitterFeed.hello() == :world
-  end
 end
