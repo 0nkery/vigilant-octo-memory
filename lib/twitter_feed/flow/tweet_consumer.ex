b82feed633571c7ea9b2b7c1847ce5bd436da0e2
Dmitry Shlagoff <wessmoke@gmail.com>
2018-03-25 13:05:19 +0700

    Implemented tweets producer & consumer

---
+++ lib/twitter_feed/flow/tweet_consumer.ex
@@ -0,0 +1,32 @@
+defmodule TwitterFeed.Flow.TweetConsumer do
+  @moduledoc """
+  Consumes Tweets from producer and saves them to database.
+  """
+
+  use ConsumerSupervisor
+
+  def start_link(arg) do
+    ConsumerSupervisor.start_link(__MODULE__, arg)
+  end
+
+  def init(_arg) do
+    children = [
+      %{id: TwitterFeed.TweetHandler, start: {TwitterFeed.TweetHandler, :start_link, []}}
+    ]
+
+    opts = [
+      strategy: :one_for_one
+      # TODO: subscribe_to
+    ]
+
+    ConsumerSupervisor.init(children, opts)
+  end
+end
+
+defmodule TwitterFeed.Flow.TweetHandler do
+  def start_link(tweet) do
+    Task.start_link(fn ->
+      TwitterFeed.Model.Tweet.upsert!(tweet)
+    end)
+  end
+end
---
+++ lib/twitter_feed/flow/tweet_producer.ex
@@ -0,0 +1,104 @@
+defmodule TwitterFeed.Flow.TweetProducerState do
+  @enforce_keys [:queue]
+  defstruct [:queue, pending_demand: 0]
+end
+
+defmodule TwitterFeed.Flow.TweetProducer do
+  @moduledoc """
+  Streams Tweets from Twitter accounts listed in database.
+  """
+
+  alias TwitterFeed.Model.{TwitterAccount, Tweet}
+  alias TwitterFeed.TwitterClient
+  alias TwitterFeed.Flow.TweetProducerState
+
+  use GenStage
+
+  def start_link() do
+    GenStage.start_link(__MODULE__, :ok)
+  end
+
+  def init(:ok) do
+    queue = :queue.new()
+
+    accounts = TwitterAccount.all()
+
+    queue =
+      Task.Supervisor.async_stream(TwitterFeed.TaskSupervisor, accounts, fn account ->
+        save_tweets_from_timeline(account)
+      end)
+      |> Enum.flat_map(fn tweets -> tweets end)
+      |> Enum.reduce(queue, fn tweet, queue -> :queue.in(tweet, queue) end)
+
+    tweet_stream =
+      Enum.map(accounts, fn account -> account.id end)
+      |> TwitterClient.stream()
+
+    {:ok, _tag} = GenStage.sync_subscribe(self(), to: tweet_stream)
+
+    {:producer_consumer, %TweetProducerState{queue: queue}}
+  end
+
+  def handle_events(tweets, _from, %TweetProducerState{queue: queue} = state) do
+    queue = Enum.reduce(tweets, queue, fn tweet, queue -> :queue.in(tweet, queue) end)
+    {:noreply, [], %{state | queue: queue}}
+  end
+
+  def handle_demand(incoming_demand, state) do
+    dispatch_events(state, incoming_demand + state.pending_demand, [])
+  end
+
+  defp dispatch_events(state, 0, tweets) do
+    {:noreply, Enum.reverse(tweets), %{state | pending_demand: 0}}
+  end
+
+  defp dispatch_events(state, demand, tweets) do
+    case :queue.out(state.queue) do
+      {{:value, tweet}, queue} ->
+        dispatch_events(%{state | queue: queue}, demand - 1, [tweet | tweets])
+
+      {:empty, queue} ->
+        {:noreply, Enum.reverse(tweets), %{state | queue: queue, pending_demand: demand}}
+    end
+  end
+
+  @spec save_tweets_from_timeline(TwitterAccount) :: list(map())
+  defp save_tweets_from_timeline(account) do
+    first = Tweet.first_for_account(account)
+    latest = Tweet.latest_for_account(account)
+
+    new_tweets_before =
+      Task.Supervisor.async(TwitterFeed.TaskSupervisor, fn ->
+        get_tweets_before_first(account, first.id)
+      end)
+
+    new_tweets_after =
+      Task.Supervisor.async(TwitterFeed.TaskSupervisor, fn ->
+        get_tweets_after_latest(account, latest.id)
+      end)
+
+    Task.await(new_tweets_before) ++ Task.await(new_tweets_after)
+  end
+
+  @spec get_tweets_before_first(TwitterAccount, Integer.t()) :: list(map())
+  defp get_tweets_before_first(account, first_id) do
+    tweets = TwitterClient.timeline_before(account.id, first_id)
+
+    if Enum.count(tweets) == 0 do
+      tweets
+    else
+      tweets ++ get_tweets_before_first(account, List.last(tweets).id)
+    end
+  end
+
+  @spec get_tweets_after_latest(TwitterAccount, Integer.t()) :: list(map())
+  defp get_tweets_after_latest(account, latest_id) do
+    tweets = TwitterClient.timeline_after(account.id, latest_id)
+
+    if Enum.count(tweets) == 0 do
+      tweets
+    else
+      tweets ++ get_tweets_after_latest(account, List.first(tweets).id)
+    end
+  end
+end
