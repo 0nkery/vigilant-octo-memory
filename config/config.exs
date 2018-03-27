48ca61de248daa63f32c38dfad37a6d89e2fa5cd
Dmitry Shlagoff <wessmoke@mail.ru>
2018-03-27 11:13:34 +0700

    Loading Twitter config from env

--- config/config.exs
+++ config/config.exs
@@ -10,4 +10,10 @@ config :twitter_feed, TwitterFeed.Repo,
 
 config :twitter_feed, ecto_repos: [TwitterFeed.Repo]
 
+config :twittex,
+       consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+       consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+       token: System.get_env("TWITTER_OWNER_TOKEN"),
+       token_secret: System.get_env("TWITTER_OWNER_TOKEN_SECRET")
+
 import_config "#{Mix.env()}.exs"
