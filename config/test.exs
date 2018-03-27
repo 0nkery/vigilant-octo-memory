17f6fc70bb9a945abd973515f1318f76dedccf1a
Dmitry Shlagoff <wessmoke@mail.ru>
2018-03-27 10:58:00 +0700

    Fixed config overrides

--- config/test.exs
+++ config/test.exs
@@ -1,3 +1,5 @@
+use Mix.Config
+
 config :twitter_feed, TwitterFeed.Repo,
        adapter: Ecto.Adapters.Postgres,
-       database: "twitter_feed_test_repo",
+       database: "twitter_feed_test_repo"
